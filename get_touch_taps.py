af23013958345f9e163ac92fd4b9acc9f44c94c0
FarooqOmerSyed <syedfarooqomer@gmail.com>
2025-05-25 21:54:10 +0530

    new_code

--- get_touch_taps.py
+++ get_touch_taps.py
@@ -50,5 +50,5 @@ def collect_touch_events(device_id):
 if __name__ == "__main__":
     # device could be here anything, realated to your usb device connected type adb devices -l to know
     # 46131JEAYL02AF 
-    device_id = "46131JEAYL02AF"
+    device_id = "36221RTJWW0LAQ"
     collect_touch_events(device_id)
